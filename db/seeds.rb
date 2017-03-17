04fe64d6913feb590e3cd6dd2d99013765878b8c
peaceInayat <peace.inayat@gmail.com>
2017-03-17 19:56:16 +0530

    Default Admin created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Admin.create(email:'admin_tushar@hh.com', password: '12345678', password_confirmation: '12345678')
